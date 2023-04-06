cb71dffd57b01037c3d4ac3d0b53020c5859cff9
haroldmud <haroldmudosa@gmail.com>
2023-04-06 13:27:54 +0300

    Feat: Finished with the Home route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 export default function App() {
   return (
     <section className=" h-[100vh]  mx-auto">
-      
-      <img src="/images/image1.png" alt="" />
+      <div className="relative">
+        <img className="h-[100vh] object-cover" src="/images/image1.png" alt="" />
+        <div className="top-12 mt-8 absolute w-full px-4 flex flex-col gap-6">
+          <h2 className="text-white font-bold text-3xl">You got the travel plans, we got the travel plan</h2>
+          <div className="text-white">
+          <p>Add adventure to your life by joining  the #vanlife movement.</p>
+          <p>Rent the perfect van to make your perfect road trip</p>
+          </div>
+          <button className="w-full bg-[#ff8c38] rounded-lg py-2 font-bold text-white">Find your van</button>
+        </div>
+      </div>
     </section>
   )
 }
