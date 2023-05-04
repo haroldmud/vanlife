67e0efb556d2e5e5d3f6fa55a21a7be23a3e89a1
haroldmud <haroldmudosa@gmail.com>
2023-05-04 12:13:24 +0200

    Feat: Add some changes from the nested branch

--- src/pages/host/list.jsx
+++ src/pages/host/list.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../../server";
 
 function List() {
-  return (
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fecthList = async () => {
+      try {
+        const res = await fetch("/api/vans");
+        const listData = await res.json();
+        setList(listData.vans);
+        setLoading(false);
+      } catch (err) {
+        setList("This is an error");
+        setLoading(false);
+      }
+    };
+    fecthList();
+    return () => {
+      fecthList();
+    };
+  }, []);
+  return loading ? (
+    <div className="text-center py-52">Loading...</div>
+  ) : (
     <div className="bg-[#fff7ed] px-4 py-8 ">
-      <div className="flex justify-between">
-        <p className="font-bold">Your listed vans</p>
-        <button>View all</button>
-      </div>
-      <div className="py-12">
-        <p className="text-center text-red-800 font-bold">
-          You haven't rented any van yet!
-        </p>
+      <h2 className="font-bold text-4xl">Your listed vans</h2>
+      <div className="py-12 flex flex-col gap-4">
+        {list.map((item, idx) => (
+          <Link
+            to={`/host/list/${idx}`}
+            key={item.id}
+            className="bg-white flex gap-4 rounded-xl p-4"
+          >
+            <img className="w-16 rounded-xl" src={item.imageUrl} alt="" />
+            <div>
+              <h3 className="font-bold text-2xl ">{item.name}</h3>
+              <p className="font-semibold text-gray-400">${item.price}/day</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
