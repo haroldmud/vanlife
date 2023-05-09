e8bee08d9dd3e737fbfccf5c1a87786ce6839e5b
haroldmud <haroldmudosa@gmail.com>
2023-05-09 15:17:38 +0200

    Feat: Handle the authentication matching

--- src/pages/authentication.jsx
+++ src/pages/authentication.jsx
@@ -1,50 +1,52 @@
 import { useEffect, useState } from "react";
-import { useLoaderData, useLocation } from "react-router-dom"
+import { useLoaderData, useLocation } from "react-router-dom";
 import { loginUser } from "../api";
 
-export default function Login(){
+export default function Login() {
   const location = useLocation();
   const logging = useLoaderData();
   const [isShown, setIsShown] = useState(true);
-  const [loginFormData, setLoginFormData] = useState({email:"", password:""});
-  const [email, setEmail] =useState('');
-  const [pass, setPass] = useState('');
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
   const [error, setError] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    setLoginFormData({email:email, password:pass})
-    
-  }
-
-   useEffect(()=>{
-    async function holdLogin(){
-      try{
-        await loginUser(loginFormData);
-        
-      }catch(err){
-        setError(true)
+    async function holdLogin() {
+      try {
+        await loginUser({ email: email, password: pass });
+      } catch (err) {
+        setError(true);
       }
     }
     holdLogin();
-    return()=>{holdLogin()}
-  },[loginFormData])
+  }
+  useEffect(()=>{
+    const handleError=setTimeout(()=>{
+      if(Error && submitted){
+        setError(false);
+        setSubmitted(false)
+      }
+    }, 3000)
+    return()=>{clearTimeout(handleError)}
+  },[submitted])
 
-  function handleEmail(event){
-    setEmail(event.target.value)
+  function handleEmail(event) {
+    setEmail(event.target.value);
   }
 
-  function handlePass(event){
-    setPass(event.target.value)
+  function handlePass(event) {
+    setPass(event.target.value);
   }
 
   useEffect(()=>{
     const handleError=setTimeout(()=>{
-      if(Error){
-        setError(false)
+      if(Error && submitted){
+        setError(false);
+        setSubmitted(false)
       }
-    }, 5000)
+    }, 3000)
     return()=>{clearTimeout(handleError)}
   },[submitted])
 
@@ -56,21 +58,69 @@ export default function Login(){
     return()=> {clearTimeout(holdIsShown)}
   },[])
 
-  return(
+  return (
     <div className="mt-12">
-      <h1 className="text-center font-bold text-2xl">Sign in to your account</h1>
-      {
-        location.state && !logging && <h2 className={`${isShown ? "text-center text-red-800 font-bold" : "text-transparent"} mt-4`}>{location.state.message}</h2>
-      }
-      <form onSubmit={(e)=>{handleSubmit(e);setSubmitted(true)}} className="flex flex-col w-10/12 mx-auto mt-16 ">
-        <input onChange={(e)=> handleEmail(e)} className="border pl-4 py-2 outline-none" placeholder="Email address" type="text" />
-        <input onChange={(e)=> handlePass(e)} className="border pl-4 py-2 outline-none" placeholder="Password" type="password" />
-        <button className="py-2 text-white font-bold bg-[#FF8C38] mt-8">Sign in</button>
+      <h1 className="text-center font-bold text-2xl">
+        Sign in to your account
+      </h1>
+      {location.state && !logging && (
+        <h2
+          className={`${
+            isShown ? "text-center text-red-800 font-bold" : "text-transparent"
+          } mt-4`}
+        >
+          {location.state.message}
+        </h2>
+      )}
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+          setSubmitted(true);
+        }}
+        className="flex flex-col w-10/12 mx-auto mt-16 "
+      >
+        <input
+          onChange={(e) => handleEmail(e)}
+          className="border pl-4 py-2 outline-none"
+          placeholder="Email address"
+          type="text"
+        />
+        <input
+          onChange={(e) => handlePass(e)}
+          className="border pl-4 py-2 outline-none"
+          placeholder="Password"
+          type="password"
+        />
+        <button className="py-2 text-white font-bold bg-[#FF8C38] mt-8">
+          Sign in
+        </button>
       </form>
-      <p className={`${error  && submitted ?"text-red-500": "text-transparent"  } text-center mt-2`}>No user with those credentials found!</p>
+      <p
+        className={`${
+          error && submitted && email.length > 0 && pass.length > 0
+            ? "text-red-500"
+            : "text-transparent"
+        } text-center mt-2`}
+      >
+        No user with those credentials found!
+      </p>
+      <p
+        className={`${
+          submitted && (email.length === 0 || pass.length === 0)
+            ? "text-red-500 text-center"
+            : "text-transparent"
+        }`}
+      >
+        No input should be empty
+      </p>
       <div className="mt-12 mb-20">
-        <p className="text-center">Don't have an account? <span className="text-[#FF8C38]"><button>Create one now</button></span></p>
+        <p className="text-center">
+          Don't have an account?{" "}
+          <span className="text-[#FF8C38]">
+            <button>Create one now</button>
+          </span>
+        </p>
       </div>
     </div>
-  )
+  );
 }
