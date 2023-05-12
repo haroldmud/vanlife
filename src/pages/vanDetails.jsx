9e650eb269ec1fdf711b14ca8feec542fccc4813
haroldmud <haroldmudosa@gmail.com>
2023-05-12 13:03:38 +0200

    Feat: Use the API end point to get details

--- src/pages/vanDetails.jsx
+++ src/pages/vanDetails.jsx
@@ -1,20 +1,39 @@
-import { useSelector } from "react-redux";
-import { Link,useParams, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams, useLocation } from "react-router-dom";
 
 function VanDetails() {
   const vanId = useParams();
   const location = useLocation();
-  console.log(location)
-  const details = useSelector((prev) => prev.vans.value.van);
-  const item = details[vanId.id.slice(0, -1)];
-  return (
+  const detail = vanId.id.slice(0, -1);
+  const [item, setItem] = useState(null);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    const vanId = async () => {
+      try {
+        const response = await fetch(`/api/vans/${Number(detail) + 1}`);
+        const vanId = await response.json();
+        setItem(vanId.vans);
+        setLoad(false)
+      } catch (error) {
+        setItem("null");
+        setLoad(false)
+      }
+    };
+    vanId();
+  }, [detail]);
+  return load ? (
+    <div className="h-[100vh] flex flex-col justify-center">
+      <p className="mx-auto  w-fit">Loading...</p>
+    </div>
+  ) : item ? (
     <div className="w-[90%] mx-auto mt-8">
       <Link to={`${location.state ? `/vans/?${location.state.search}` : "/vans"}`}
        className="mb-4 flex gap-1">
         <span className="my-auto text-gray-500">&larr;</span>
         <p className="underline text-sm my-auto"> Go back</p>
       </Link>
-      <img className="object-cover rounded-md " src={item.imageUrl} alt="Van" />
+      <img className="object-cover rounded-md " src={item?.imageUrl} alt="Van" />
       <div className="mt-8">
         <div className="flex flex-col gap-2 mb-4">
           <div
@@ -43,7 +62,7 @@ function VanDetails() {
         </button>
       </div>
     </div>
-  );
+  ):<>Not data</>
 }
 
 export default VanDetails;
